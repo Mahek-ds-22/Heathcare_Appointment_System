043029c35561c0d80409bddd5f97ce2593ca7b23
Mahek-ds-22 <mahek6845@gmail.com>
2025-09-19 00:39:36 +0530

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html','./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#eef6ff',
+          100: '#dff0ff',
+          500: '#2563eb'
+        },
+        accent: '#f97316'
+      }
+    }
+  },
+  plugins: []
+}
